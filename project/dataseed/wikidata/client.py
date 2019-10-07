8b08d9266a0d4f7d2c0b6dc63192254d99ac6649
Tobias Schoch <Tobias.Schoch@helbling.ch>
2019-10-07 18:19:47 +0200

    added wikidata client

---
+++ project/dataseed/wikidata/client.py
@@ -0,0 +1,22 @@
+import logging
+from io import StringIO
+
+import requests
+import pandas as pd
+
+log = logging.getLogger(__name__)
+
+
+class WikiDataClient:
+    URL = 'https://query.wikidata.org/sparql'
+
+    @classmethod
+    def query(cls, query_text):
+        r = requests.get(cls.URL, params={'format': 'json', 'query': query_text})
+        data = r.json()
+        bindings = data['results']['bindings']
+        data = pd.DataFrame.from_dict(map(lambda row: {k: row[k]["value"] for k in row.keys()}, bindings))
+        io = StringIO()
+        data.to_csv(io,index=False)
+        io.seek(0)
+        return pd.read_csv(io)
---
+++ project/server/api/categories.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# created: 07.10.2019
+# author:  TOS
+
+import logging
+
+log = logging.getLogger(__name__)
+
