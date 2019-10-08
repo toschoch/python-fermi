b439a8bcec8075b3fcd76c29e7b8323f5246725e
Tobias Schoch <tobias.schoch@vtxmail.ch>
2019-10-08 10:06:10 +0200

    datetime parsing in wikidataclient

--- project/dataseed/wikidata/client.py
+++ project/dataseed/wikidata/client.py
@@ -3,6 +3,7 @@ from io import StringIO
 
 import requests
 import pandas as pd
+import numpy as np
 
 log = logging.getLogger(__name__)
 
@@ -15,8 +16,14 @@ class WikiDataClient:
         r = requests.get(cls.URL, params={'format': 'json', 'query': query_text})
         data = r.json()
         bindings = data['results']['bindings']
+        datetime_columns = [c for row in bindings for c in row.keys() if row[c].get('datatype','').split('#')[-1]=='dateTime']
+        datetime_columns = np.unique(datetime_columns).tolist()
         data = pd.DataFrame.from_dict(map(lambda row: {k: row[k]["value"] for k in row.keys()}, bindings))
         io = StringIO()
-        data[r.json()['head']['vars']].to_csv(io,index=False)
+        requested_vars = r.json()['head']['vars']
+        data[data.columns.intersection(requested_vars)].to_csv(io,index=False)
         io.seek(0)
-        return pd.read_csv(io)
+        if len(datetime_columns) > 0:
+            return pd.read_csv(io, parse_dates=datetime_columns)
+        else:
+            return pd.read_csv(io)
